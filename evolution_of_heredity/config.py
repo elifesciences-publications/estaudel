e443842d99a0b8965122f137b118f389b8fa0266
Guilhem Doulcier <guilhem.doulcier@ens.fr>
2019-11-07 09:30:36 +0100

    Figure update for arxiv submission

--- evolution_of_heredity/config.py
+++ evolution_of_heredity/config.py
@@ -1,13 +1,20 @@
 from matplotlib.colors import ListedColormap, LinearSegmentedColormap
+import os
 
 PANEL_DICT = dict(xy=(1, 1), xycoords="axes fraction",
                  ha='center', va='center',
                  bbox=dict(boxstyle="round",
                            ec=(.1, 0.1, .1),
-                           fc=(1., 1, 1),
-                 ))
+                           fc=(1., 1, 1),))
+
+ 
 FIGPATH = 'fig'
 SUPFIGPATH = 'supfig'
+
+for path in [FIGPATH,SUPFIGPATH]:
+    if not os.path.exists(path):
+        os.mkdir(path)
+
 FONT_DICT  ={'fontweight': 'normal', 'size':'x-large'}
 
 
